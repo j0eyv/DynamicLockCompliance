d57245ac672b0e30c75852c257eb2627afc12f2f
j0eyv <41282854+j0eyv@users.noreply.github.com>
2022-10-11 12:12:38 +0200

    Add files via upload

---
+++ CC_Detection_DyamicLock.ps1
@@ -0,0 +1,14 @@
+﻿#Determine the current state of EnableGoodby (Dynamic Lock)
+    $Registry = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name EnableGoodbye
+    $Value = "1"
+
+    If ($Registry -eq $Value){
+    $Value = "1"
+    } 
+    else
+    {
+    $Value = "0"
+    }
+
+$hash = @{ EnableGoodbye = $value}
+return $hash | ConvertTo-Json -Compress
